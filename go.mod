9fd9cd875f8207e2e9efd059c882aef5ea30d3f2
Modularise <modularise@modularise.com>
2020-03-05 12:54:00 +0000

    Splice from github.com/prometheus/prometheus@7164b58945bb

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/modularise/prometheus-tsdb
+
+go 1.13
+
+require (
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/cespare/xxhash v1.1.0
+	github.com/dgryski/go-sip13 v0.0.0-20190329191031-25c5027a8c7b
+	github.com/go-kit/kit v0.10.0
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/golang/snappy v0.0.1
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/oklog/ulid v1.3.1
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.4.1
+	github.com/prometheus/common v0.9.1
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/sys v0.0.0-20200212091648-12a6c2dcc1e4
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
+
+replace k8s.io/klog => github.com/simonpasquier/klog-gokit v0.1.0
---
+++ pkg/labels/labels_test.go
@@ -0,0 +1,455 @@
+// Copyright 2019 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package labels
+
+import (
+	"testing"
+
+	"github.com/modularise/prometheus-tsdb/internal/residuals/util/testutil"
+)
+
+func TestLabels_MatchLabels(t *testing.T) {
+	labels := Labels{
+		{
+			Name:	"__name__",
+			Value:	"ALERTS",
+		},
+		{
+			Name:	"alertname",
+			Value:	"HTTPRequestRateLow",
+		},
+		{
+			Name:	"alertstate",
+			Value:	"pending",
+		},
+		{
+			Name:	"instance",
+			Value:	"0",
+		},
+		{
+			Name:	"job",
+			Value:	"app-server",
+		},
+		{
+			Name:	"severity",
+			Value:	"critical",
+		},
+	}
+
+	tests := []struct {
+		providedNames	[]string
+		on		bool
+		expected	Labels
+	}{
+		// on = true, explicitly including metric name in matching.
+		{
+			providedNames: []string{
+				"__name__",
+				"alertname",
+				"alertstate",
+				"instance",
+			},
+			on:	true,
+			expected: Labels{
+				{
+					Name:	"__name__",
+					Value:	"ALERTS",
+				},
+				{
+					Name:	"alertname",
+					Value:	"HTTPRequestRateLow",
+				},
+				{
+					Name:	"alertstate",
+					Value:	"pending",
+				},
+				{
+					Name:	"instance",
+					Value:	"0",
+				},
+			},
+		},
+		// on = false, explicitly excluding metric name from matching.
+		{
+			providedNames: []string{
+				"__name__",
+				"alertname",
+				"alertstate",
+				"instance",
+			},
+			on:	false,
+			expected: Labels{
+				{
+					Name:	"job",
+					Value:	"app-server",
+				},
+				{
+					Name:	"severity",
+					Value:	"critical",
+				},
+			},
+		},
+		// on = true, explicitly excluding metric name from matching.
+		{
+			providedNames: []string{
+				"alertname",
+				"alertstate",
+				"instance",
+			},
+			on:	true,
+			expected: Labels{
+				{
+					Name:	"alertname",
+					Value:	"HTTPRequestRateLow",
+				},
+				{
+					Name:	"alertstate",
+					Value:	"pending",
+				},
+				{
+					Name:	"instance",
+					Value:	"0",
+				},
+			},
+		},
+		// on = false, implicitly excluding metric name from matching.
+		{
+			providedNames: []string{
+				"alertname",
+				"alertstate",
+				"instance",
+			},
+			on:	false,
+			expected: Labels{
+				{
+					Name:	"job",
+					Value:	"app-server",
+				},
+				{
+					Name:	"severity",
+					Value:	"critical",
+				},
+			},
+		},
+	}
+
+	for i, test := range tests {
+		got := labels.MatchLabels(test.on, test.providedNames...)
+		testutil.Equals(t, test.expected, got, "unexpected labelset for test case %d", i)
+	}
+}
+
+func TestLabels_HasDuplicateLabelNames(t *testing.T) {
+	cases := []struct {
+		Input		Labels
+		Duplicate	bool
+		LabelName	string
+	}{
+		{
+			Input:		FromMap(map[string]string{"__name__": "up", "hostname": "localhost"}),
+			Duplicate:	false,
+		}, {
+			Input: append(
+				FromMap(map[string]string{"__name__": "up", "hostname": "localhost"}),
+				FromMap(map[string]string{"hostname": "127.0.0.1"})...,
+			),
+			Duplicate:	true,
+			LabelName:	"hostname",
+		},
+	}
+
+	for i, c := range cases {
+		l, d := c.Input.HasDuplicateLabelNames()
+		testutil.Equals(t, c.Duplicate, d, "test %d: incorrect duplicate bool", i)
+		testutil.Equals(t, c.LabelName, l, "test %d: incorrect label name", i)
+	}
+}
+
+func TestLabels_WithoutEmpty(t *testing.T) {
+	tests := []struct {
+		input		Labels
+		expected	Labels
+	}{
+		{
+			input: Labels{
+				{
+					Name:	"__name__",
+					Value:	"test",
+				},
+				{
+					Name: "foo",
+				},
+				{
+					Name:	"hostname",
+					Value:	"localhost",
+				},
+				{
+					Name: "bar",
+				},
+				{
+					Name:	"job",
+					Value:	"check",
+				},
+			},
+			expected: Labels{
+				{
+					Name:	"__name__",
+					Value:	"test",
+				},
+				{
+					Name:	"hostname",
+					Value:	"localhost",
+				},
+				{
+					Name:	"job",
+					Value:	"check",
+				},
+			},
+		},
+		{
+			input: Labels{
+				{
+					Name:	"__name__",
+					Value:	"test",
+				},
+				{
+					Name:	"hostname",
+					Value:	"localhost",
+				},
+				{
+					Name:	"job",
+					Value:	"check",
+				},
+			},
+			expected: Labels{
+				{
+					Name:	"__name__",
+					Value:	"test",
+				},
+				{
+					Name:	"hostname",
+					Value:	"localhost",
+				},
+				{
+					Name:	"job",
+					Value:	"check",
+				},
+			},
+		},
+	}
+
+	for i, test := range tests {
+		got := test.input.WithoutEmpty()
+		testutil.Equals(t, test.expected, got, "unexpected labelset for test case %d", i)
+	}
+}
+
+func TestLabels_Equal(t *testing.T) {
+	labels := Labels{
+		{
+			Name:	"aaa",
+			Value:	"111",
+		},
+		{
+			Name:	"bbb",
+			Value:	"222",
+		},
+	}
+
+	tests := []struct {
+		compared	Labels
+		expected	bool
+	}{
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bbb",
+					Value:	"222",
+				},
+				{
+					Name:	"ccc",
+					Value:	"333",
+				},
+			},
+			expected:	false,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bar",
+					Value:	"222",
+				},
+			},
+			expected:	false,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bbb",
+					Value:	"233",
+				},
+			},
+			expected:	false,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bbb",
+					Value:	"222",
+				},
+			},
+			expected:	true,
+		},
+	}
+
+	for i, test := range tests {
+		got := Equal(labels, test.compared)
+		testutil.Equals(t, test.expected, got, "unexpected comparison result for test case %d", i)
+	}
+}
+
+func TestLabels_Compare(t *testing.T) {
+	labels := Labels{
+		{
+			Name:	"aaa",
+			Value:	"111",
+		},
+		{
+			Name:	"bbb",
+			Value:	"222",
+		},
+	}
+
+	tests := []struct {
+		compared	Labels
+		expected	int
+	}{
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"110",
+				},
+				{
+					Name:	"bbb",
+					Value:	"222",
+				},
+			},
+			expected:	1,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bbb",
+					Value:	"233",
+				},
+			},
+			expected:	-1,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bar",
+					Value:	"222",
+				},
+			},
+			expected:	1,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bbc",
+					Value:	"222",
+				},
+			},
+			expected:	-1,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+			},
+			expected:	1,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bbb",
+					Value:	"222",
+				},
+				{
+					Name:	"ccc",
+					Value:	"333",
+				},
+				{
+					Name:	"ddd",
+					Value:	"444",
+				},
+			},
+			expected:	-2,
+		},
+		{
+			compared: Labels{
+				{
+					Name:	"aaa",
+					Value:	"111",
+				},
+				{
+					Name:	"bbb",
+					Value:	"222",
+				},
+			},
+			expected:	0,
+		},
+	}
+
+	for i, test := range tests {
+		got := Compare(labels, test.compared)
+		testutil.Equals(t, test.expected, got, "unexpected comparison result for test case %d", i)
+	}
+}
